7d8e932f9870901b2703b6cfebe8b6921b34d5fc
andy_tsai <andy_tsai@paradise-soft.com.tw>
2024-01-10 11:02:43 +0800

    feat: install 套件 & color system

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-nesting": {},
+    "postcss-pxtorem": {
+      rootValue: 16,
+      propList: ["*"],
+    },
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
-}
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,42 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#BF9D7D',
+          tiny: '#FAF7F5',
+          40: '#F1EAE4',
+          60: '#E1D1C2',
+          80: '#D0B79F',
+          120: '#7B6651'
+        },
+        success: {
+          DEFAULT: '#52DD7E',
+          10: '#E8FEE7',
+          20: '#BCFBBD',
+          120: '#299F65', 
+        },
+        info: {
+          DEFAULT: '#3BADEF',
+          10: '#E6FBFE',
+          20: '#B1EFFD',
+          120: '#1D66AC',
+        },
+        error: {
+          DEFAULT: '#DA3E51',
+          10: '#FDECEF',
+          20: '#F5CCD1',
+          120: '#C22538',
+        },
+        black: {
+          DEFAULT: '#000000',
+          10: '#F9F9F9',
+          40: '#ECECEC',
+          60: '#909090',
+          80: '#4B4B4B',
+          dark: '#140F0A',
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
